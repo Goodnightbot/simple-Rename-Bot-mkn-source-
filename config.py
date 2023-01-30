155f229643efdaa1f3cc3315141f84c6190f6b31
Goodnightbot <118749160+Goodnightbot@users.noreply.github.com>
2023-01-30 12:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "25055544"))
+API_HASH = environ.get("API_HASH", "11bc08bc32ee6e05e46655a1e34435d6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5948650770:AAG2Fy-qlEGQ98vLFS2ED9S0lv8_-O8fAfA")
+ADMIN = int(environ.get("ADMIN", "5428829672"))          
+CAPTION = environ.get("CAPTION", "@kwicbotupdates")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/e13400bbf2408df0a1fd0.jpg")
