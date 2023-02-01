4bc310a1131a9a9d714718a768540a62b87236d6
Goodnightbot <118749160+Goodnightbot@users.noreply.github.com>
2023-02-01 15:09:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "25055544"))
 API_HASH = environ.get("API_HASH", "11bc08bc32ee6e05e46655a1e34435d6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5948650770:AAG2Fy-qlEGQ98vLFS2ED9S0lv8_-O8fAfA")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5922081257:AAGbGtkXncCaG64v3p7pEec2xZ1xU-lrloA")
 ADMIN = int(environ.get("ADMIN", "5428829672"))          
-CAPTION = environ.get("CAPTION", "@kwicbotupdates")
+CAPTION = environ.get("CAPTION", "@Suppotgroup")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/e13400bbf2408df0a1fd0.jpg")
